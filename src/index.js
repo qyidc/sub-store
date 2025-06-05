f233b2bae45e03846546d0da25f27c898da4c1bb
qyidc <yakoo_@126.com>
2025-06-06 07:48:17 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-<script type="javascript" title="Cloudflare Worker (worker.js)">
+<script type="javascript" title="Cloudflare Worker (index.js)">
 // This is the content for your Worker script (e.g., worker.js or src/index.js)
 // Ensure you have `wrangler.toml` configured correctly, especially R2 bindings.
 
