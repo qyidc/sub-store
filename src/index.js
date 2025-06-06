5d01c472495b04f287d40c759d6eb43b7e6dfcb9
qyidc <yakoo_@126.com>
2025-06-06 21:53:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,26 +4,6 @@ import {
     parseSingleLink
 } from './utils.js';
 
-function generateFullClashConfig(proxies, env) {
-    let yamlConfig = 'proxies:\n';
-    proxies.forEach(proxy => {
-        yamlConfig += `  - name: ${proxy.name}\n`;
-        yamlConfig += `    type: ${proxy.type}\n`;
-        yamlConfig += `    server: ${proxy.server}\n`;
-        yamlConfig += `    port: ${proxy.port}\n`;
-        if (proxy.cipher) {
-            yamlConfig += `    cipher: ${proxy.cipher}\n`;
-        }
-        if (proxy.password) {
-            yamlConfig += `    password: ${proxy.password}\n`;
-        }
-        if (proxy.udp) {
-            yamlConfig += `    udp: ${proxy.udp}\n`;
-        }
-    });
-    return yamlConfig;
-}
-
 export default {
     async fetch(request, env, ctx) {
         const url = new URL(request.url);
